d309e0e102e3196633cbcdcf55a52e6bb205ab9c
JoseAzmitia <azmitiajc@gmail.com>
2023-03-15 15:37:46 -0600

    Se agrega el componente Main

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Edison es el mejor</Text>
-      <StatusBar style="auto" />
+      <Text>InventoryHub</Text>
     </View>
   );
 }
