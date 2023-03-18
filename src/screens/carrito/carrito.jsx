138e8fa71159ba1510ddc4a00964b1fa23b929d3
JoseAzmitia <azmitiajc@gmail.com>
2023-03-18 17:51:45 -0600

    Pantallas base

---
+++ src/screens/carrito/carrito.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { View, Text } from 'react-native';
+import Constants from 'expo-constants';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+  },
+});
+
+function PantallaCarrito() {
+  return (
+    <View style={styles.container}>
+      <Text>Carrito</Text>
+    </View>
+  );
+}
+
+export default PantallaCarrito;
---
+++ src/screens/productos/productos.jsx
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { View, Text } from 'react-native';
+import Constants from 'expo-constants';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+  },
+});
+
+function PantallaProductos() {
+  return (
+    <View style={styles.container}>
+      <Text>Productos</Text>
+    </View>
+  );
+}
+
+export default PantallaProductos;
