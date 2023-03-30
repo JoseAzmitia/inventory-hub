7e4e7edd234b59a07ef2a842f11897faf62cf2b3
JoseAzmitia <azmitiajc@gmail.com>
2023-03-30 15:19:31 -0600

    iconos tabnavigator

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,7 +1,8 @@
+/* eslint-disable react/no-unstable-nested-components */
 // eslint-disable-next-line no-unused-vars
 import React, { useContext } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import { NavigationContainer } from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 // pantallas
 import PantallaSumario from '../screens/Sumario';
@@ -14,12 +15,65 @@ const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Sumario" component={PantallaSumario} />
-      <Tab.Screen name="Productos" component={PantallaProductos} />
-      <Tab.Screen name="Ventas" component={PantallaVentas} />
-      <Tab.Screen name="Carrito" component={PantallaCarrito} />
-      <Tab.Screen name="Salir" component={Salir} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: { backgroundColor: '#FFFFFF' },
+        tabBarInactiveTintColor: '#828282',
+        tabBarActiveTintColor: '#27AE60',
+      }}
+    >
+      <Tab.Screen
+        name="Sumario"
+        component={PantallaSumario}
+        options={{
+          title: 'Inicio',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Productos"
+        component={PantallaProductos}
+        options={{
+          title: 'Productos',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-grid-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Ventas"
+        component={PantallaVentas}
+        options={{
+          title: 'Ventas',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-analytics-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Carrito"
+        component={PantallaCarrito}
+        options={{
+          title: 'Carrito',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-cart-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Salir"
+        component={Salir}
+        options={{
+          title: 'Salir',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-log-in-outline" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
