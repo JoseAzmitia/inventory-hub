31a3afdfb12ae28cfc060f9d6b3623d30fba2e8b
JoseAzmitia <azmitiajc@gmail.com>
2023-04-22 16:00:52 -0600

    Modal en pantalla salir

--- src/screens/salir.jsx
+++ src/screens/salir.jsx
@@ -1,21 +1,43 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { View, Text } from 'react-native';
+import Modal from 'react-native-modal';
+import globalStyles from '../styles/GlobalStyles';
 import { AuthContext } from '../context/authContext';
 import BtnApp from '../components/Btn';
 
-function Salir() {
+function Salir({ navigation }) {
   const { logout } = useContext(AuthContext);
+  const [modal, setModal] = useState(false);
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontFamily: 'Inter-Regular', fontSize: 20, marginBottom: 30 }}>
-        ¿Seguro que quieres cerrar sesión?
-      </Text>
+      <Modal isVisible={modal}>
+        <View style={globalStyles.modalContainer}>
+          <Text style={globalStyles.modalText}>¿Confirma cerrar la sesión?</Text>
+          <BtnApp
+            texto="Sí, cerrar sesión"
+            onPress={() => {
+              setModal(false);
+              logout();
+            }}
+          />
+          <BtnApp
+            texto="Cancelar"
+            newColor
+            color="#FF7575"
+            onPress={() => {
+              setModal(false);
+              navigation.navigate('Sumario');
+            }}
+          />
+        </View>
+      </Modal>
+      <Text style={globalStyles.modalText}>¿Desea salir de la sesión?</Text>
       <BtnApp
         texto="Salir"
         onPress={() => {
-          logout();
+          setModal(true);
         }}
       />
     </View>
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://192.168.0.16:8080/api';
+export const API_URL = '';
 export const ImageTemplate =
   'https://img.freepik.com/vector-premium/icono-linea-concepto-producto-ilustracion-elemento-simple-diseno-simbolo-esquema-concepto-producto-puede-utilizar-ui-ux-web-movil_159242-2076.jpg';
