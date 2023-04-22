75e44fc4567e18adc8afe22150eaa5c48053aacd
JoseAzmitia <azmitiajc@gmail.com>
2023-04-22 17:00:33 -0600

    Actualizado .gitignore para ignorar config.js

--- src/utils/config.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = '';
-export const ImageTemplate =
-  'https://img.freepik.com/vector-premium/icono-linea-concepto-producto-ilustracion-elemento-simple-diseno-simbolo-esquema-concepto-producto-puede-utilizar-ui-ux-web-movil_159242-2076.jpg';
