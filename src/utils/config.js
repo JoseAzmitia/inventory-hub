9dc140fcfd01c6ac348a3222ff069307556122f6
JoseAzmitia <azmitiajc@gmail.com>
2023-08-02 23:19:38 -0600

    Eliminado archivo config.js

--- src/utils/config.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = 'http:159.223.188.55:8080/api';
-export const ImageTemplate =
-  'https://img.freepik.com/vector-premium/icono-linea-concepto-producto-ilustracion-elemento-simple-diseno-simbolo-esquema-concepto-producto-puede-utilizar-ui-ux-web-movil_159242-2076.jpg';
